03ba6e67d176aa20bff6d6b66a331edb88dc01b4
balazs21mate <balazs21mate@gmail.com>
2022-07-15 21:18:51 +0200

    add hamburger menu

--- english_world_ui/src/components/Navbar.js
+++ english_world_ui/src/components/Navbar.js
@@ -1,18 +1,32 @@
+import React, {useState} from 'react'
 import './navbar.css'
+import {MenuIcon} from '@heroicons/react/solid'
+import {XCircleIcon} from '@heroicons/react/outline'
 
-function Navbar(){
+function Navbar(props){
+    const [navbarMenu, setNavbarMenu] = useState(false)
+
+    const set_navbar_menu_true = () => {
+        setNavbarMenu(true)
+    }
+
+    const set_navbar_menu_false = () => {
+        setNavbarMenu(false)
+    }
+
     return(
         <div className="w-screen fixed top-0 p-5 text-secondary_color flex flex-col bg-primery">
             <div className="flex justify-between ">
-                <a href="/" className="text-3xl">English world</a>
-                <div className="hidden md:flex text-2xl">
+                <a href="/" className="text-6xl">English world</a>
+                <div className="hidden md:flex items-center text-2xl">
                     <a className="mr-2 hover:border-b-solid hover:border-b-2 hover:border-b-secondary_color" href="/">Cards</a>
                     <a className="mr-2 hover:border-b-solid hover:border-b-2 hover:border-b-secondary_color" href="/">Practice</a>
                 </div>
+                <MenuIcon className={`w-7 ${navbarMenu? "hidden": "block"} md:hidden`} onClick={set_navbar_menu_true}/>
+                <XCircleIcon className={`w-7 ${navbarMenu? "block": "hidden"} md:hidden`} onClick={set_navbar_menu_false}/>
             </div>
-            <div className="flex flex-col items-center my-2 md:hidden text-2xl">
+            <div className={`${navbarMenu? "flex": "hidden"} flex-col items-center my-2 md:hidden text-2xl`}>
                 <a className="hover:border-b-solid hover:border-b-2 hover:border-b-secondary_color" href="/">Cards</a>
-                <hr></hr>
                 <a className="hover:border-b-solid hover:border-b-2 hover:border-b-secondary_color" href="/">Practice</a>
             </div>
         </div>
