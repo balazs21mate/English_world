6c6b81ca9a99b6346f0bdfb4f0cf3bfbace7e7c8
balazs21mate <balazs21mate@gmail.com>
2022-07-16 23:52:42 +0200

    turn card

--- english_world_ui/src/components/cards/Card.js
+++ english_world_ui/src/components/cards/Card.js
@@ -6,7 +6,7 @@ function Card({title, list}){
     const [rotate, setRotate] = useState(false)
     const [buttonDisabled, setButtonDisabled] = useState(false)
     const [mirrorText, setMirrorText] = useState(false)
-    const [any, setAny] = useState(true)
+    const [mirrorTextReturn, setMirrorTextReturn] = useState(true)
 
     useEffect(()=>{
         setText(rotate?list[counter]?.hungarian:list[counter]?.english)
@@ -19,7 +19,7 @@ function Card({title, list}){
     const set_rotate=()=>{
         if (rotate === false) {
             setButtonDisabled(true)
-            setAny(!any)
+            setMirrorTextReturn(!mirrorTextReturn)
             setText("")
             setRotate(!rotate)
             setTimeout(()=>{
@@ -30,7 +30,7 @@ function Card({title, list}){
         }else{
             setButtonDisabled(true)
             setText("")
-            setAny(!any)
+            setMirrorTextReturn(!mirrorTextReturn)
             setTimeout(() => {
                 setMirrorText(!mirrorText)
                 setRotate(!rotate)
@@ -48,7 +48,7 @@ function Card({title, list}){
         <div className='flex flex-col justify-center items-center h-[20rem] max-w-lg w-[90%]'>
             <h1 className='mb-4 text-secondary_color text-[1.5rem] font-light'>{title}</h1>
             <small></small>
-            <div className={`${rotate? 'rotate_card':'rotate_card_origin'} ${mirrorText&&'rotate_card_null'} ${any&&'rotate_card_180'} shadow-card flex justify-center items-center text-secondary_color w-full h-full border-solid border border-black rounded-lg mb-8`} onClick={buttonDisabled?null:set_rotate}>{text}</div>
+            <div className={`${rotate? 'rotate_card':'rotate_card_origin'} ${mirrorText&&'rotate_card_null'} ${mirrorTextReturn&&'rotate_card_180'} shadow-card flex justify-center items-center text-secondary_color w-full h-full border-solid border border-black rounded-lg mb-8`} onClick={buttonDisabled?null:set_rotate}>{text}</div>
             <button className="text-center max-w-[25rem] my-0 mx-auto p-1 text-3xl bg-[rgba(163,133,158,0.5)] text-white border-none rounded-xl shadow-button cursor-pointer outline-none mb-8 tracking-[0.3rem]" onClick={new_counter} disabled={buttonDisabled}>Next</button>
         </div>
     )
