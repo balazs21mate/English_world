3e8d06e51b71db4a3038641130a926862b7f7cc1
balazs21mate <balazs21mate@gmail.com>
2022-08-04 12:33:39 +0200

    change Form.js style

--- english_world_ui/src/components/pages/create_cards/Form.js
+++ english_world_ui/src/components/pages/create_cards/Form.js
@@ -2,8 +2,6 @@ import React, {useContext, useEffect, useState} from "react";
 
 import {CreateListContext} from '../../context/CreateList';
 
-import {PlusIcon} from '@heroicons/react/solid'
-
 function Form() {
 
     const [title, setTitle] = useState('');
@@ -60,7 +58,7 @@ function Form() {
                 <input type='text' className="w-full  mt-2 border outline-none border-secondary_color rounded p-2" placeholder="Title..." onChange={handleTitle} value={title}/>
             </label>
             <hr className='w-[80%] mt-10 mb-2 border-b-solid self-center border-secondary_color'></hr>
-            <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="flex flex-col xl:flex-row justify-between items-center">
                 <div className="flex flex-col">
                     <label className="text-xl mt-1">English:
                         <input type='text' className="w-full outline-none mt-2 border border-secondary_color rounded p-2" placeholder="English..." onChange={handleInput(setEnglish)} value={english}/>
@@ -69,8 +67,7 @@ function Form() {
                         <input type='text' className="w-full outline-none mt-2 border border-secondary_color rounded p-2" placeholder="Hungarian..." onChange={handleInput(setHungarian)} value={hungarian}/>
                     </label>
                 </div>
-                <button type="button" className={`hidden lg:block text-center w-20 p-1 bg-button text-white border-none rounded-xl shadow-button outline-none`} onClick={handleForm}>Add new word</button>
-                <button type="button" className={`lg:hidden text-center w-12 mt-2 p-1 bg-button text-white border-none rounded-xl shadow-button outline-none`} onClick={handleForm}><PlusIcon /></button>
+                <button type="button" className={`text-center w-25 h-14 mt-2 p-2 bg-button text-white border-none rounded-xl shadow-button outline-none`} onClick={handleForm}>Add new word</button>
             </div>
             <hr className='w-[80%] mt-6 border-b-solid self-center border-secondary_color'></hr>
             <button type="button" className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-3xl bg-button text-white border-none rounded-xl shadow-button outline-none mb-8 tracking-[0.3rem]" onClick={()=>{setTitle('');handleForm()}}>Send</button>
