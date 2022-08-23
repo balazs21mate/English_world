dcea872bf7236c8e9976dfe8b2b492153527f959
balazs21mate <balazs21mate@gmail.com>
2022-08-23 11:10:34 +0200

    improve Form.js

--- english_world_ui/src/components/pages/create_cards/Form.js
+++ english_world_ui/src/components/pages/create_cards/Form.js
@@ -12,8 +12,8 @@ function Confirm({text, func, visible, set_visible}) {
         <div className={`${visible?'flex':'hidden'} absolute left-10 bg-white flex-col w-[80%] border rounded p-2 border-secondary_color mx-auto`}>
             <p className="text-center text-secondary_color text-lg">{text}</p>
             <div className="flex justify-between">
-                <button className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-3xl bg-green-600 text-white border-none rounded-xl shadow-button outline-none mb-8 tracking-[0.3rem]" onClick={confirm}>Yes</button>
-                <button className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-3xl bg-red-600 text-white border-none rounded-xl shadow-button outline-none mb-8 tracking-[0.3rem]" onClick={()=>set_visible(false)}>No</button>
+            <button className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>set_visible(false)}>Cancel</button>
+                <button className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-lg bg-button text-white border-none rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={confirm}>Confirm</button>
             </div>
         </div>
     )
@@ -72,7 +72,7 @@ function Form() {
             setTitles(list =>[...list, Item.title]);
             set_error(`Created this list: ${title}!`, 1000, true);
         } else {
-            set_error('Please, fill in all fields!', 1000, false);
+            set_error('Please, fill in all fields!', 2000, false);
         }
     }
 
@@ -108,7 +108,7 @@ function Form() {
                 set_error(`Created this list: ${title}!`, 1000, true);
             }
         } else {
-            set_error('Please, fill in all fields!', 1000, false);
+            set_error('Please, fill in all fields!', 2000, false);
         }
     }
 
@@ -159,9 +159,11 @@ function Form() {
                     setEnglish('');
                     setHungarian('');
                 } else {
-                    set_error(`This word: ${english} isn't in this list: ${title}`, 1000, false);
+                    set_error(`This word: ${english} isn't in this list: ${title}`, 3000, false);
                 }
             }
+        }else{
+            set_error('Please, fill in all fields!', 2000, false);
         }
     }
 
@@ -177,7 +179,7 @@ function Form() {
                     setEnglish('');
                     setHungarian('');
                 } else {
-                    set_error(`This word: ${english} isn't in this list: ${title}`, 1000, false);
+                    set_error(`This word: ${english} isn't in this list: ${title}`, 3000, false);
                 }
             }
         }
