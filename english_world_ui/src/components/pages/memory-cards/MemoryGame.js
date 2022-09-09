24fcd47c7971fe10265a2eefe42b6ce04571fa60
balazs21mate <balazs21mate@gmail.com>
2022-09-09 23:07:41 +0200

    set grid cols in memory game

--- english_world_ui/src/components/pages/memory-cards/MemoryGame.js
+++ english_world_ui/src/components/pages/memory-cards/MemoryGame.js
@@ -11,18 +11,24 @@ function MemoryGame(){
     const [localList, setLocalList] = useState([]);
     const [numberOfCards, setNumberOfCards] = useState(8);
     const [numberOfCols, setNumberOfCols] = useState(4);
+    const [numberOfColsMobile, setNumberOfColsMobile] = useState(4);
     const [gridCols, setGridCols] = useState({});
+    const [gridColsMobile, setGridColsMobile] = useState({});
 
     useEffect(()=>{
         const cols = {
+            4:'md:grid-cols-4',
+            5:'md:grid-cols-5',
+            6:'md:grid-cols-6',
+            7:'md:grid-cols-7'
+        };
+        const colsMobile = {
             2:'grid-cols-2',
             4:'grid-cols-4',
-            5:'grid-cols-5',
-            6:'grid-cols-6',
-            7:'grid-cols-7'
         };
 
         setGridCols(cols);
+        setGridColsMobile(colsMobile);
     }, [setGridCols])
 
     useEffect(()=>{
@@ -36,9 +42,9 @@ function MemoryGame(){
         setLocalList(list);
     },[memoryWordsList, numberOfCards])
 
-    const handleLayout = (numCol, numCards) => {
+    const handleLayout = (func, numCol, numCards) => {
         setNumberOfCards(numCards);
-        setNumberOfCols(numCol);
+        func(numCol);
     }
 
     const handleNewGame = () => {
@@ -60,19 +66,19 @@ function MemoryGame(){
             <PagesTitle title='Memory Game'/>
             <h1 className='text-3xl'>Level:</h1>
             <div className='hidden md:flex'>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(4,6);handleNewGame()}}>4x3</button>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(4,8);handleNewGame()}}>4x4</button>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(5,10);handleNewGame()}}>5x4</button>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(6,15);handleNewGame()}}>6x5</button>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(6,18);handleNewGame()}}>6x6</button>
-                <button className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(7,21);handleNewGame()}}>7x6</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfCols,4,6);handleNewGame()}}>4x3</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfCols,4,8);handleNewGame()}}>4x4</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfCols,5,10);handleNewGame()}}>5x4</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfCols,6,15);handleNewGame()}}>6x5</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfCols,6,18);handleNewGame()}}>6x6</button>
+                <button className="text-center max-w-[25rem] mt-6 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfCols,7,21);handleNewGame()}}>7x6</button>
             </div>
             <div className='flex md:hidden'>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(2,2);handleNewGame()}}>2x2</button>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(4,6);handleNewGame()}}>4x3</button>
-                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(4,8);handleNewGame()}}>4x4</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfColsMobile,2,2);handleNewGame()}}>2x2</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfColsMobile,4,6);handleNewGame()}}>4x3</button>
+                <button className="text-center max-w-[25rem] mt-6 mr-2 mx-auto p-1 text-lg text-button border border-button rounded-lg outline-none mb-8 tracking-[0.3rem]" onClick={()=>{handleLayout(setNumberOfColsMobile,4,8);handleNewGame()}}>4x4</button>
             </div>
-            <div className={`grid ${numberOfCols?gridCols[numberOfCols]:'grid-cols-4'} gap-1`}>
+            <div className={`grid ${numberOfColsMobile?gridColsMobile[numberOfColsMobile]:'grid-cols-4'} ${numberOfCols?gridCols[numberOfCols]:'md:grid-cols-4'} gap-1`}>
                 {
                     localList.map((item,index) => <MemoryCard key={index} item={item}/>)
                 }
